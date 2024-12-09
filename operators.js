70a8157874ae563d141aaac16042fbc68d1586a1
vedanth515 <vedanthkarnataka@gmail.com>
2024-12-09 18:18:04 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,198 @@
+// var a = 10;
+// var b = 50;
+// console.log(a+b);
+// console.log(b-a);
+
+// var t = true
+// var f = false
+// console.log(true/false);
+
+// if(false){
+//     console.log("hello i am vedanth");
+//     console.log("today is very cool");
+//     console.log("today is very cool");
+//     console.log("today is very cool");
+//     console.log("today is very cool");
+//     console.log("today is very cool");
+
+// }else{
+//     console.log("not satisfied");   
+// }
+
+
+// if(1){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// if([]==[]){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// if({}){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// if(x=10){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// var a = 100;
+// var b = 200;
+// if(a<b){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// if(a>b){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// var a = 90;
+// var b = 100;
+
+// if(a<=b){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// var a = 90;
+// var b = 100;
+
+// if(a>=b){
+//     console.log("hey its ture");
+
+// }else{
+//     console.log("hey its false");
+
+// }
+
+// var age = 17;
+
+// var userinput=prompt("enter your age here")
+
+// if(userinput>=18){
+//     console.log("you are eligible");
+    
+// }else{
+//     console.log("you are not eligible");
+    
+// }
+
+
+//  true/false  ? "yes" : "no"  
+
+//  1 ? console.log("im ture value"):console.log("im false value");
+
+// var age = 17;
+
+// var userinput=prompt("enter your age")
+
+// if(userinput>=17){
+//     console.log("you are eligible to vote");
+    
+// }else{
+//     console.log("you are not eligible to vote");
+    
+// }
+
+// var age = 21;
+
+// var userinput=prompt("enter your age here")
+
+// if(userinput>=21){
+//     console.log("you are eligible to marriage");
+    
+// }else{
+//     console.log("you are not eligible to marriage");
+    
+// }
+
+// var age = 18;
+
+// var userinput=prompt("enter your age here")
+
+// if(userinput>=18){
+//     console.log("you are eligible to marriage");
+    
+// }else{
+//     console.log("you are not eligible to marriage");
+    
+// }
+
+// var age = 18;
+
+// var userinput=prompt("apply for a driving license")
+
+// if(userinput>=18){
+//     console.log("you are eligible to apply driving license");
+    
+// }else{
+//     console.log("you are not eligible to apply driving license");
+    
+// }
+
+// var attendance = 75
+
+// var userinput=prompt("student is eligible for an exam based on their attendance percentage")
+
+// if(userinput>=75){
+//     console.log("you are eligible for the exam");
+    
+// }else{
+//     console.log("you are not eligible for the exam");
+    
+// }
+
+// var age = 60;
+
+// var userinput=prompt("if a person qualifies for a senior citizen discount")
+
+// if(userinput>=60){
+//     console.log("he will get senior citizen discount");
+    
+// }else{
+//     console.log("he will get not senior citizen discount");
+    
+// }
+
+
+// var a = 0.1 + 0.2;
+// var b = 0.3;
+// var z = a + b;
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
